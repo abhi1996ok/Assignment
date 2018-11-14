6f80012e8382e6ae7b7d0fe40893bd30db05456f
abhi1996ok <abhi1996ok@gmail.com>
2018-11-15 00:32:22 +0530

    finally the listview in the fragment_dashboard is commpleted using DashBoard.java and fragment_dashboard.xml

--- app/src/main/java/com/example/abhishek/licco/DashBoard.java
+++ app/src/main/java/com/example/abhishek/licco/DashBoard.java
@@ -6,12 +6,44 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class DashBoard extends Fragment {
+    ListView listView;
+    ListView listView2;
+
+    String names[]={
+            "Law",
+            "Law",
+            "Literature",
+            "Literature",
+            "Entertainment",
+            "Art",
+            "Law",
+            "Law",
+            "Literature",
+            "Literature",
+            "Entertainment",
+            "Art",
+            "Law",
+            "Law",
+            "Literature",
+            "Literature",
+            "Entertainment",
+            "Art",
+    };
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View view =inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        listView=view.findViewById(R.id.listView);
+        listView2 = view.findViewById(R.id.listView2);
+
+        CustomAdapter adapter = new CustomAdapter(getActivity(),names);
+        listView.setAdapter(adapter);
+        listView2.setAdapter(adapter);
+        return view;
     }
 }
 
