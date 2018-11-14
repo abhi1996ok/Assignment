b82d4587007d8caf35bec3026da4f8c88e4083a7
abhi1996ok <abhi1996ok@gmail.com>
2018-11-15 00:25:39 +0530

    the CustomAdapter and CustomLayout is made for the custom listview in the dashboard fragment's card 5 and card 6

---
+++ app/src/main/java/com/example/abhishek/licco/CustomAdapter.java
@@ -0,0 +1,41 @@
+package com.example.abhishek.licco;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomAdapter extends ArrayAdapter {
+    Context mContext;
+    Activity activity;
+    String name[];
+
+    public CustomAdapter(Activity activity, String name[])
+    {
+        super(activity,R.layout.custom_layout,name);
+        this.activity=activity;
+          this.name=name;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater=activity.getLayoutInflater();
+         View v =inflater.inflate(R.layout.custom_layout,null);
+
+
+           TextView textView = v.findViewById(R.id.text);
+
+            textView.setText(name[position]);
+
+
+
+        return v;
+    }
+}
