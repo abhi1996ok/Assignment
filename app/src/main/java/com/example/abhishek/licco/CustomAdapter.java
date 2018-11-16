bf90cf5fdf665502b9a1ba29ac38adaf8ff7c6c9
rjtvr <rajatverma1963@gmail.com>
2018-11-16 14:54:25 +0530

    go threw this and user risk find

--- app/src/main/java/com/example/abhishek/licco/CustomAdapter.java
+++ app/src/main/java/com/example/abhishek/licco/CustomAdapter.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CustomAdapter extends ArrayAdapter {
@@ -20,21 +19,14 @@ public class CustomAdapter extends ArrayAdapter {
         super(activity,R.layout.custom_layout,name);
         this.activity=activity;
           this.name=name;
-
     }
-
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater=activity.getLayoutInflater();
-         View v =inflater.inflate(R.layout.custom_layout,null);
-
-
-           TextView textView = v.findViewById(R.id.text);
-
-            textView.setText(name[position]);
-
-
+        View v = inflater.inflate(R.layout.custom_layout,null);
+        TextView textView = v.findViewById(R.id.text);
+        textView.setText(name[position]);
 
         return v;
     }
