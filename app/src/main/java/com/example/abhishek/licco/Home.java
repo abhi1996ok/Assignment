d294ab18d7e9848a499c765a0e910e89a2ac37fd
abhi1996ok <abhi1996ok@gmail.com>
2018-11-22 18:18:24 +0530

    The new option_menu is added to the activity

--- app/src/main/java/com/example/abhishek/licco/Home.java
+++ app/src/main/java/com/example/abhishek/licco/Home.java
@@ -10,10 +10,17 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
